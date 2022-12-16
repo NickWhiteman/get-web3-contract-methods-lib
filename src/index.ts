7dd1935a86d5d71247c31afc8da11e18bb9f29e9
Nikita Romanov <romanov_nikita@hotmail.com>
2022-12-16 11:14:54 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { getMethodsContract } from "./contract";
 import { initialEventsContract } from "./eventsContract";
+import dotenv from "dotenv";
+dotenv.config();
 
 export { getMethodsContract, initialEventsContract };
