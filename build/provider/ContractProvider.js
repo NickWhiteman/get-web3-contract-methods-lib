bf0694015495b3bb6f28cc1517a5d7469e1a1344
Nikita Romanov <romanov_nikita@hotmail.com>
2023-01-05 06:16:48 +0300

    fix build

--- build/provider/ContractProvider.js
+++ build/provider/ContractProvider.js
@@ -1,57 +1,59 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { parseEther } from "ethers/lib/utils";
 import { createContext, useEffect, useState } from "react";
-import { getMethodsContract } from "../contract/contract";
+import { getMethodsContract } from "src/contract/contract";
 import { initialEventsContract } from "src/contract/eventsContract";
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const { ethereum } = window;
-export const ContractContext = createContext({});
-export const ContractProvider = ({ children, ABI, eventName, behaviorEvents }) => {
-    const [account, setAccount] = useState();
-    const contractMethods = getMethodsContract(CONTRACT_ADDRESS, ABI);
-    const gasLimit = parseEther("0.0000000000021");
-    const getAccount = () => {
-        if (account) {
-            return account;
-        }
+export const useContextEthers = function () {
+    const ContractContext = createContext({});
+    return ({ children, ABI, eventName, behaviorEvents }) => {
+        const [account, setAccount] = useState("");
+        const contractMethods = getMethodsContract(CONTRACT_ADDRESS, ABI);
+        const gasLimit = parseEther("0.0000000000021");
+        const getAccount = () => {
+            if (account) {
+                return account;
+            }
+        };
+        const contract = () => {
+            return contractMethods;
+        };
+        const connectWallet = async () => {
+            try {
+                if (!ethereum)
+                    throw new Error("Please install MetaMask.");
+                const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+                setAccount(accounts[0]);
+            }
+            catch (error) {
+                console.log(error);
+                throw new Error("No ethereum object");
+            }
+        };
+        const checkIfWalletIsConnect = async () => {
+            try {
+                connectWallet();
+            }
+            catch (error) {
+                console.log(error);
+            }
+        };
+        const contextFunctions = {
+            contract,
+            getAccount,
+            connectWallet,
+        };
+        useEffect(() => {
+            checkIfWalletIsConnect();
+            initialEventsContract({
+                behaviorEvents,
+                contractAddress: CONTRACT_ADDRESS,
+                ABI,
+                eventName,
+            });
+        }, [account]);
+        return _jsx(ContractContext.Provider, { value: contextFunctions, children: children });
     };
-    const contract = () => {
-        return contractMethods;
-    };
-    const connectWallet = async () => {
-        try {
-            if (!ethereum)
-                console.log("Please install MetaMask.");
-            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-            setAccount(accounts[0]);
-        }
-        catch (error) {
-            console.log(error);
-            throw new Error("No ethereum object");
-        }
-    };
-    const checkIfWalletIsConnect = async () => {
-        try {
-            connectWallet();
-        }
-        catch (error) {
-            console.log(error);
-        }
-    };
-    const contextFunctions = {
-        contract,
-        getAccount,
-        connectWallet,
-    };
-    useEffect(() => {
-        checkIfWalletIsConnect();
-        initialEventsContract({
-            behaviorEvents,
-            contractAddress: CONTRACT_ADDRESS,
-            ABI,
-            eventName,
-        });
-    }, [account]);
-    return _jsx(ContractContext.Provider, { value: contextFunctions, children: children });
 };
 //# sourceMappingURL=ContractProvider.js.map
